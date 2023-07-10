82b9888340ef1f76d6fb3e281cf94f6cac8fbc32
Teenkung123 <suraritnuthaphon@gmail.com>
2023-07-11 01:05:48 +0700

    Initial commit

---
+++ src/main/java/com/teenkung/enderenigma/EnderEnigma.java
@@ -0,0 +1,17 @@
+package com.teenkung.enderenigma;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnderEnigma extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
